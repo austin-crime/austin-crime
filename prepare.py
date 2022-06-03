4cc71868b60d783f49cb8e6a55ca6a15dec0912e
Oliver Ton <oliver.ton75@gmail.com>
2022-06-03 09:52:36 -0500

    add some docstrings

--- prepare.py
+++ prepare.py
@@ -1,13 +1,14 @@
 import pandas as pd 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
-import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import MinMaxScaler
+
 
 def attribute_nulls(df):
+    '''
+    This function takes in a dataframe and checking all the rows if they have any missing values
+    and return a dataframe with the number of missing values and percent missing.
+    '''
     nulls = df.isnull().sum()
     rows = len(df)
     percent_missing = nulls / rows 
@@ -15,6 +16,10 @@ def attribute_nulls(df):
     return dataframe
 
 def column_nulls(df):
+    '''
+    This function takes in a dataframe and checking all the columns if they have any missing values
+    and return a dataframe with with the number of missing value and percent missing.
+    '''
     new_df = pd.DataFrame(df.isnull().sum(axis=1), columns = ['cols_missing']).reset_index()\
     .groupby('cols_missing').count().reset_index().\
     rename(columns = {'index': 'rows'})
@@ -22,6 +27,9 @@ def column_nulls(df):
     return new_df
     
 def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
+    '''
+    This function takes in a dataframe and then drop the missing values.
+    '''
     threshold = int(round(prop_required_column*len(df.index),0))
     df.dropna(axis=1, thresh=threshold, inplace=True)
     threshold = int(round(prop_required_row*len(df.columns),0))
@@ -49,104 +57,33 @@ def split_data(df):
     return train, validate, test
 
 #remove outliers
-
-def remove_outliers(df, k, col_list):
-    ''' remove outliers from a list of columns in a dataframe 
-        and return that dataframe
+def get_lower_and_upper_bounds(df,col, k=1.5):
     '''
-    
-    for col in col_list:
-
-        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
-        
-        iqr = q3 - q1   # calculate interquartile range
-        
-        upper_bound = q3 + k * iqr   # get upper bound
-        lower_bound = q1 - k * iqr   # get lower bound
-
-        # return dataframe without outliers
-        
-        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
-        
-    return df
-
-# Scale data after splitting with MinMax
-def scale_data(train, validate, test, columns_to_scale, return_scaler=False):
+    takes in a df, a column from the df, and a multiplier (default is 1.5)
+    calculates the IQR, prints out the lower and upper bound, and returns entries 
+    below the lower bound, and higher than the upper bound
     '''
-    Scales the 3 data splits.
-    
-    takes in the train, validate, and test data splits and returns their scaled counterparts.
-    
-    If return_scaler is true, the scaler object will be returned as well.
-    '''
-    
-    train_scaled = train.copy()
-    validate_scaled = validate.copy()
-    test_scaled = test.copy()
-    
-    scaler = MinMaxScaler()
-    scaler.fit(train[columns_to_scale])
-    
-    train_scaled[columns_to_scale] = scaler.transform(train[columns_to_scale])
-    validate_scaled[columns_to_scale] = scaler.transform(validate[columns_to_scale])
-    test_scaled[columns_to_scale] = scaler.transform(test[columns_to_scale])
-    
-    if return_scaler:
-        return scaler, train_scaled, validate_scaled, test_scaled
-    else:
-        return train_scaled, validate_scaled, test_scaled
-
-def visualize_scaler(scaler, df, target_columns, bins=10):
-    fig, axs = plt.subplots(len(target_columns), 2, figsize=(15, 12))
-    df_scaled = df.copy()
-    df_scaled[target_columns] = scaler.fit_transform(df[target_columns])
-    for (ax1, ax2), col in zip(axs, target_columns):
-        ax1.hist(df[col], bins=bins)
-        ax1.set(title=f'{col} before scaling', xlabel=col, ylabel='count')
-        ax2.hist(df_scaled[col], bins=bins)
-        ax2.set(title=f'{col} after scaling with {scaler.__class__.__name__}', xlabel=col, ylabel='count')
-    # set the spacing between subplots
-    plt.subplots_adjust(left=0.1,
-                        bottom=0.05, 
-                        right=0.9, 
-                        top=0.98, 
-                        wspace=0.4, 
-                        hspace=0.4)
-    plt.tight_layout()
-    return fig, axs
-
+    q1 = df[col].quantile(.25)
+    q3 = df[col].quantile(.75)
+    iqr = q3 - q1 # calculate interquartile range
+    upper_value = q3 + (k * iqr) # get upper bound
+    lower_value = q1 - (k * iqr) # get lower bound
+    #return dataframe without outliers
+    under_bound = df[df[col] < lower_value]
+    over_bound = df[df[col] > upper_value]
+    print(f'for {col}, the lower bound is {lower_value}, and the upper bound is {upper_value}')
+    return under_bound, over_bound
 
 ######### Cleaning Steps ##############
 
 def prep_data(df):
     
-    df = reduce_time_frame(df)
-    df = drop_columns(df)
-    df = remove_rows_missing_values(df)
-    df = impute_missing_values(df)
-    df = rename_columns(df)
-    df = rename_values(df)
-    df = cast_column_types(df)
-    df = engineer_features(df)
-    
-    return df 
-
-################################################################################
-
-def reduce_time_frame(df: pd.DataFrame) -> pd.DataFrame:
-
     # Change occurrence date to datetime type in order to subeset data
     df.occ_date = pd.to_datetime(df.occ_date, format = '%Y-%m-%d')
     
     # Subset the data to include observations between 2018-01-01 and 2021-12-31.
     df = df[(df.occ_date >= '2018-01-01') & (df.occ_date <= '2021-12-31')]
-
-    return df
-
-################################################################################
-
-def drop_columns(df: pd.DataFrame) -> pd.DataFrame:
-
+    
     # These are all the columns that will be dropped from the dataframe.
     columns = [
         'incident_report_number',
@@ -167,13 +104,7 @@ def drop_columns(df: pd.DataFrame) -> pd.DataFrame:
     
     # Drop duplicated information and unncessary/unuseful columns
     df = df.drop(columns = columns)
-
-    return df
-
-################################################################################
-
-def remove_rows_missing_values(df: pd.DataFrame) -> pd.DataFrame:
-
+    
     # Here we'll drop rows with missing values that cannot be reasonabled imputed with a value.
     null_columns = [
     'clearance_status',
@@ -186,23 +117,11 @@ def remove_rows_missing_values(df: pd.DataFrame) -> pd.DataFrame:
     
     for column in null_columns:
         df = df[~df[column].isna()]
-
-    return df
-
-################################################################################
-
-def impute_missing_values(df: pd.DataFrame) -> pd.DataFrame:
-
+    
     # Here we'll fill missing values for some columns with a value we have decided upon.
     df['location_type'] = df.location_type.fillna('OTHER / UNKNOWN') #filling all na with the Other/Unknown value
     df['council_district'] = df.council_district.fillna(9) # filling all na with the most common district
-
-    return df
-
-################################################################################
-
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
-
+    
     # Renaming columns for clarity and readability
     
     mapper = {
@@ -212,13 +131,7 @@ def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     'rep_date_time' : 'report_time'}
     
     df = df.rename(columns = mapper)
-
-    return df
-
-################################################################################
-
-def rename_values(df: pd.DataFrame) -> pd.DataFrame:
-
+    
     # Changing clearance status values to a more readable version using the data documentation 
     
     clearance_mapper = {
@@ -227,13 +140,7 @@ def rename_values(df: pd.DataFrame) -> pd.DataFrame:
     'C' : 'cleared by arrest'}
     
     df['clearance_status'] = df.clearance_status.map(clearance_mapper)
-
-    return df
-
-################################################################################
-
-def cast_column_types(df: pd.DataFrame) -> pd.DataFrame:
-
+    
     # Changing data to numeric types where appropriate
     df.latitude = df.latitude.astype('float')
     df.longitude = df.longitude.astype('float')
@@ -247,19 +154,14 @@ def cast_column_types(df: pd.DataFrame) -> pd.DataFrame:
     df.occurrence_time = pd.to_datetime(df.occurrence_time, format = '%Y-%m-%dT%H:%M:%S')
     df.report_time = pd.to_datetime(df.report_time, format = '%Y-%m-%dT%H:%M:%S')
 
-    return df
-
-################################################################################
-
-def engineer_features(df: pd.DataFrame) -> pd.DataFrame:
-    
     # Create new target variable with True or False values where "not cleared" 
     # is False and "cleared by arrest" and "cleared by exception" are True.
     clearance = np.where(df.clearance_status == 'not cleared', False, True)
     df['cleared'] = clearance
+    df.cleared.value_counts()
+    
+    return df 
 
-    # Create a feature that is the difference between when a crime occurred and 
-    # when it was reported.
-    df['time_to_report'] = df.report_time - df.occurrence_time
+    
 
-    return df
+
