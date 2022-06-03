438506d13598b7867d442bce7aaed4acf054b883
Oliver Ton <oliver.ton75@gmail.com>
2022-06-03 09:55:49 -0500

    make little change to split function

--- prepare.py
+++ prepare.py
@@ -36,24 +36,22 @@ def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75
     df.dropna(axis=0, thresh=threshold, inplace=True)
     return df
 
-
-def split_data(df):
-
-    """
-    This function takes in a dataframe, then splits and returns the data as train, validate, and test sets 
-    using random state 42.
-    """
-    # split data into 2 groups, train_validate and test, assigning test as 20% of the dataset
-    train_validate, test = train_test_split(
-        df, test_size=0.2, random_state=42, stratify=df["cleared"]
-    )
-    # split train_validate into 2 groups with
-    train, validate = train_test_split(
-        train_validate,
-        test_size=0.3,
-        random_state=42,
-        stratify=train_validate["cleared"],
-    )
+#Split the data into train, validate and test to ensure the data not leakage 
+def split_data(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
     return train, validate, test
 
 #remove outliers
